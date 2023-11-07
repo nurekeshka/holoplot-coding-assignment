df00024fa1bb1b9a94f84de69fc9dfa0274495ce
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2023-11-07 15:28:18 +0100

    feat: add string representation for positions

--- src/utils.rs
+++ src/utils.rs
@@ -1,7 +1,15 @@
+use std::fmt;
+
 pub struct Position {
     pub x: f32,
     pub y: f32,
     pub z: f32,
+}
+
+impl fmt::Display for Position {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "({}, {}, {})", self.x, self.y, self.z)
+    }
 }
 
 impl Clone for Position {
