091f284a5517625c13719182871bc2b606d20281
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2023-11-07 15:05:40 +0100

    feat: add clone implementation for audio object manager

--- src/managers.rs
+++ src/managers.rs
@@ -7,3 +7,14 @@ pub struct AudioObjectManager {
     current: usize,
     counter: usize,
 }
+
+impl Clone for AudioObjectManager {
+    fn clone(&self) -> AudioObjectManager {
+        AudioObjectManager {
+            history: self.history.clone(),
+            current: self.current,
+            objects: self.objects.clone(),
+            counter: self.counter,
+        }
+    }
+}
