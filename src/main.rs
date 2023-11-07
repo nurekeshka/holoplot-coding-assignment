ddbb12786c7fa512222008fd5e56748611e0c1a2
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2023-11-07 15:54:26 +0100

    feat: add coding assignment testing script

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,24 @@ mod managers;
 mod utils;
 
 use crate::constants::Coordinate;
-use crate::entities::AudioObject;
 use crate::managers::AudioObjectManager;
 
 fn main() {
-    let mut audio_manager: managers::AudioObjectManager = AudioObjectManager::new();
+    let mut manager: AudioObjectManager = AudioObjectManager::new();
+
+    let id_1: usize = manager.add(Coordinate::A.position().unwrap()).id;
+    let id_2: usize = manager.add(Coordinate::B.position().unwrap()).id;
+    manager.move_object(id_1, Coordinate::C.position().unwrap());
+    let id_3: usize = manager.add(Coordinate::D.position().unwrap()).id;
+    manager.move_object(id_2, Coordinate::E.position().unwrap());
+    manager.remove(id_1);
+    manager.move_object(id_3, Coordinate::F.position().unwrap());
+    manager.move_object(id_3, Coordinate::G.position().unwrap());
+    manager.undo();
+    manager.undo();
+
+    manager.undo();
+    manager.redo();
+
+    println!("{}", manager.to_string());
 }
